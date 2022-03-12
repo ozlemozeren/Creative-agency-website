9c47ecb021b9002d6b0ca7632bb8227938e51567
ozlemozeren <ozlmcsknzrn@gmail.com>
2022-03-12 11:01:51 +0300

    last commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const toggle = document.querySelector(".toggle");
+
+const navigation = document.querySelector(".navigation");
+
+toggle.addEventListener("click", () => {
+  toggle.classList.toggle("active");
+  navigation.classList.toggle("active");
+});
